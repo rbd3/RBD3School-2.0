e13b142f005b76b70e0be87eb9053f11666f22c1
Andry Narson <119520276+rbd3@users.noreply.github.com>
2024-02-15 10:50:13 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or created .
 #
 # Example:
 #
