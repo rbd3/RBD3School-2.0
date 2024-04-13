b0abd4006e717ea226a69ef92014991ae9626a12
rbd3 <rabedesanaandry01@gmail.com>
2024-04-13 16:11:14 -0600

    Add user table

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+
+  validates :first_name, presence: true
+  validates :role, inclusion: { in: %w[admin user director teacher], message: '%<value>s is not a valid role [admin, user]' }
+end
