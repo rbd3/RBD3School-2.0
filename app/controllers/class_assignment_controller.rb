0b83d6d058458dbacfffc8b8e1c5afc54ef7745e
rbd3 <rabedesanaandry01@gmail.com>
2024-04-22 11:29:54 -0600

    Add create class method

--- app/controllers/class_assignment_controller.rb
+++ app/controllers/class_assignment_controller.rb
@@ -3,4 +3,16 @@ class ClassAssignmentController < ApplicationController
     def index
         @class = ClassAssignment.all
       end
+
+      def create
+        @class = ClassAssignment.new(class_assignment_params)
+        if @class.save
+          render json: @class, status: :created
+        else
+          render json: @class.errors, status: :unprocessable_entity
+        end
+
+      def class_assignment_params
+        params.require(:class_assignment).permit(:class_name)
+      end
 end
