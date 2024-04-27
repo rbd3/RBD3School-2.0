78e96e7bbe881ff81d926524363f9ac6ca3beacd
rbd3 <rabedesanaandry01@gmail.com>
2024-04-27 17:03:04 -0600

    Update student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,14 +10,18 @@ class Student < ApplicationRecord
     total_mark = 0
     total_coefficient = 0
 
+  
     marks.includes(:subject).each do |mark|
-      coefficient = mark.subject.subject_coefficient.coefficient
-      total_mark += mark.mark_value * coefficient
-      total_coefficient += coefficient
+      coefficient = mark.subject.subject_coefficients.first&.coefficient
+      total_mark += mark.mark_value * coefficient.to_i
+      
+
+      total_coefficient += coefficient.to_i
     end
-
+  
     average = total_coefficient.zero? ? 0 : total_mark.to_f / total_coefficient
-
-    { total_mark:, average: }
+  
+    { total_mark: total_mark, average: average }
   end
+  
 end
