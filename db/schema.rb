a1091d1c261fd29e991bdc293f581121173e86c2
rbd3 <rabedesanaandry01@gmail.com>
2024-04-28 14:23:52 -0600

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_222407) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_28_202203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_222407) do
     t.bigint "class_assignment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "matricule"
     t.index ["class_assignment_id"], name: "index_students_on_class_assignment_id"
   end
 
