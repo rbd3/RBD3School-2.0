188c7eb3f87a156555cf46ff605a326ae1131802
rbd3 <rabedesanaandry01@gmail.com>
2024-04-29 10:32:06 -0600

    Add teaching assignement api

---
+++ test/controllers/api/teaching_assignments_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class Api::TeachingAssignmentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_teaching_assignments_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_teaching_assignments_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_teaching_assignments_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_teaching_assignments_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_teaching_assignments_destroy_url
+    assert_response :success
+  end
+end
