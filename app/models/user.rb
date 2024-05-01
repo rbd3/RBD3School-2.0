143933c650223f93285cf0cc4e6ab78116b56940
rbd3 <rabedesanaandry01@gmail.com>
2024-05-01 11:53:29 -0600

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  has_one :teacher
-  has_one :student
+  has_one :teacher, dependent: :destroy
+  has_one :student, dependent: :destroy
 
-  validates :email, :password_digest, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+
   validates :first_name, presence: true
   validates :role,
             inclusion: { in: %w[admin user director teacher student staff],
