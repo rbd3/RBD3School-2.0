3ff93d2060bad995c8dee1faab90ae5d74ea759c
rbd3 <rabedesanaandry01@gmail.com>
2024-05-01 08:09:57 -0600

    Update signup user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -19,15 +19,21 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  # POST /api/users
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
+# POST /api/users
+def create
+  @user = User.new(user_params)
+  if @user.save
+    if @user.role == 'teacher'
+      @teacher = Teacher.create(user: @user, first_name: @user.first_name, last_name: @user.last_name, subject_taught: params[:subject_taught])
+      render json: { user: @user, teacher: @teacher }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: @user, status: :created
     end
+  else
+    render json: @user.errors, status: :unprocessable_entity
   end
+end
+
 
   # PATCH/PUT /api/user/:id
   def update
@@ -58,7 +64,7 @@ class Api::UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
 
-    if @user&.authenticate(params[:password])
+    if @user&.authenticate(params[:password_digest])
       render json: { message: 'Login successful', user: @user }
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
@@ -70,6 +76,6 @@ class Api::UsersController < ApplicationController
   # Only allow a list of trusted parameters through.
   def user_params
     params[:user][:role]&.downcase!
-    params.require(:user).permit(:first_name, :last_name, :email, :role, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :role, :password_digest)
   end
 end
