95c0d0048ab0688af49e8d81d66754f8c438dfa1
rbd3 <rabedesanaandry01@gmail.com>
2024-05-01 08:26:12 -0600

    Link user to teacher and student

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -26,6 +26,9 @@ def create
     if @user.role == 'teacher'
       @teacher = Teacher.create(user: @user, first_name: @user.first_name, last_name: @user.last_name, subject_taught: params[:subject_taught])
       render json: { user: @user, teacher: @teacher }, status: :created
+    elsif @user.role == 'student' # Corrected here
+      @student = Student.create(user: @user, first_name: @user.first_name, last_name: @user.last_name, email: @user.email, matricule: params[:matricule])
+      render json: { user: @user, student: @student }, status: :created
     else
       render json: @user, status: :created
     end
@@ -33,6 +36,7 @@ def create
     render json: @user.errors, status: :unprocessable_entity
   end
 end
+
 
 
   # PATCH/PUT /api/user/:id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_one :teacher
   has_one :student
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, :password_digest, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :role,
-            inclusion: { in: %w[admin user director teacher staff],
-                         message: '%<value>s is not a valid role [admin, user, director, teacher staff]' }
+            inclusion: { in: %w[admin user director teacher student staff],
+                         message: '%<value>s is not a valid role [admin, user, director, teacher. student, staff]' }
 end
