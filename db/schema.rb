31fbbfe831910bc6348b860cf56a3c24d74c472c
rbd3 <rabedesanaandry01@gmail.com>
2024-05-01 11:40:00 -0600

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_145606) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_173646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,13 +106,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_145606) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "userable_type"
     t.bigint "userable_id"
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "password"
+    t.string "password_confirmation"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["userable_type", "userable_id"], name: "index_users_on_userable"
   end
 
